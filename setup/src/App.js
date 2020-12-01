1c594e6c570adb681f92a59b8abbbbf76dcc8ee2
Suhaiboon <suhaibtareq@gmail.com>
2020-12-01 07:45:33 -0500

    Project still incomplete
    When delete button clicked all items get deleted.
    Edit button through a big fat error.
    
    Will go back later to check it again.

--- setup/src/App.js
+++ setup/src/App.js
@@ -2,9 +2,17 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -24,7 +32,7 @@ function App() {
       //deal with edit
       setList(
         list.map((item) => {
-          if (item.id === editItem) {
+          if (item.id === editID) {
             return { ...item, title: name };
           }
           return item;
@@ -59,12 +67,17 @@ function App() {
     setList(list.filter((item) => item.id !== id));
   };
 
+  //Still running into issues when I click edit.
   const editItem = (id) => {
     const specificItem = list.find((item) => item.id === id);
     setIsEditing(true);
     setEditID(id);
     setName(specificItem.title);
   };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   return (
     <section className="section-center">
